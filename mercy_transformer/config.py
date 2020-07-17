de92cebea47519bf923311ce350d9bcc497d8b85
chagmgang <chagmgang@gmail.com>
2020-07-17 17:50:19 +0900

    add albert to config

--- mercy_transformer/config.py
+++ mercy_transformer/config.py
@@ -2,4 +2,5 @@ from transformers import *
 
 MODELS = {
     'bert': [BertModel, BertTokenizer, 'bert-base-multilingual-uncased'],
-    'distilbert': [DistilBertModel, DistilBertTokenizer, 'distilbert-base-multilingual-cased']}
+    'distilbert': [DistilBertModel, DistilBertTokenizer, 'distilbert-base-multilingual-cased'],
+    'albert': [AlbertModel, AlbertTokenizer, 'albert-base-v1']}
